7c479122aa5d14ab2f6a5443181daa7039a395e7
srikarcyfy <srikarm@cyfy.ai>
2026-01-07 08:24:29 +0530

    scheduler cors fix

--- components/scheduler.tsx
+++ components/scheduler.tsx
@@ -41,6 +41,9 @@ const Scheduler = () => {
         try {
           const response = await fetch(scriptURL, {
             method: "POST",
+            headers: {
+              "Content-Type": "text/plain;charset=utf-8",
+            },
             body: JSON.stringify({
               action: "check", // Tell script to check slots
               date: format(selectedDate, 'yyyy-MM-dd')
